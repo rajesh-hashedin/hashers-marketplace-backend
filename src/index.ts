ae982793b71e091bc33142e2826564a61e5ca44d
rajesh-hashedin <rcharhajari@deloitte.com>
2024-07-31 17:37:48 +0530

    routes setup

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,9 @@
 import express, { Express, Request, Response } from "express";
 import { PORT } from "./secrets";
+import rootRouter from "./routes";
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("working");
-});
+app.use("/api", rootRouter);
 
 app.listen(PORT, () => console.log("App working"));
