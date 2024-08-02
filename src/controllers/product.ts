075f1e0c7d16cfdf6f4d950f6d58911a07fae7d9
rajesh-hashedin <rcharhajari@deloitte.com>
2024-08-02 10:32:42 +0530

    try catch statement added

--- src/controllers/product.ts
+++ src/controllers/product.ts
@@ -8,30 +8,35 @@ interface JwtPayload {
 }
 
 export const getProducts = async (req: Request, res: Response) => {
-  const products = await await prismaClient.product.findMany();
-  const token = req.headers["authorization"]?.split(" ")[1]!;
-  const decoded = jwt.verify(token, JWT_SECRET) as JwtPayload;
-  const newProducts = [];
-  for (const product of products) {
-    const transaction = await prismaClient.transaction.findFirst({
-      where: {
-        buyerId: decoded.userId,
-        productId: product.id,
-      },
-    });
-    newProducts.push({
-      ...product,
-      ownProduct: product.ownerId === decoded.userId,
-      alreadySent: !!transaction,
-    });
-  }
+  try {
+    const products = await prismaClient.product.findMany();
+    const token = req.headers["authorization"]?.split(" ")[1]!;
+    const decoded = jwt.verify(token, JWT_SECRET) as JwtPayload;
+    const newProducts = [];
+    for (const product of products) {
+      const transaction = await prismaClient.transaction.findFirst({
+        where: {
+          buyerId: decoded.userId,
+          productId: product.id,
+        },
+      });
+      newProducts.push({
+        ...product,
+        ownProduct: product.ownerId === decoded.userId,
+        alreadySent: !!transaction,
+      });
+    }
 
-  res.status(200).send(newProducts);
+    res.status(200).send(newProducts);
+  } catch (e) {
+    console.log(e);
+    res.status(500).send("Internal server error");
+  }
 };
 
 export const addProduct = async (req: Request, res: Response) => {
-  const { name, price, description, imageLink } = req.body;
   try {
+    const { name, price, description, imageLink } = req.body;
     if (!name || !price || !imageLink) {
       res
         .status(400)
@@ -66,114 +71,133 @@ export const addProduct = async (req: Request, res: Response) => {
 };
 
 export const updateProduct = async (req: Request, res: Response) => {
-  const { id, name, price, description } = req.body;
-  if (!id) {
-    res.status(400).send({ message: "Product id is required" });
-    return;
+  try {
+    const { id, name, price, description } = req.body;
+    if (!id) {
+      res.status(400).send({ message: "Product id is required" });
+      return;
+    }
+    if (!name && !price && !description) {
+      res
+        .status(400)
+        .send({ message: "Atleast one field is required to update product" });
+      return;
+    }
+    let product = await prismaClient.product.findFirst({ where: { id } });
+    if (!product) {
+      res.status(400).send({ message: "Product not found" });
+      return;
+    }
+    const token = req.headers["authorization"]?.split(" ")[1]!;
+    const decoded = jwt.verify(token, JWT_SECRET) as JwtPayload;
+    product = await prismaClient.product.findFirst({
+      where: { id, ownerId: decoded.userId },
+    });
+    if (!product) {
+      res
+        .status(401)
+        .send({ message: "You are not authorized to update this product" });
+      return;
+    }
+    product = await prismaClient.product.update({
+      where: { id, ownerId: decoded.userId },
+      data: {
+        name,
+        price,
+        description,
+      },
+    });
+    res.status(200).send(product);
+  } catch (e) {
+    console.log(e);
+    res.status(500).send("Internal server error");
   }
-  if (!name && !price && !description) {
-    res
-      .status(400)
-      .send({ message: "Atleast one field is required to update product" });
-    return;
-  }
-  let product = await prismaClient.product.findFirst({ where: { id } });
-  if (!product) {
-    res.status(400).send({ message: "Product not found" });
-    return;
-  }
-  const token = req.headers["authorization"]?.split(" ")[1]!;
-  const decoded = jwt.verify(token, JWT_SECRET) as JwtPayload;
-  product = await prismaClient.product.findFirst({
-    where: { id, ownerId: decoded.userId },
-  });
-  if (!product) {
-    res
-      .status(401)
-      .send({ message: "You are not authorized to update this product" });
-    return;
-  }
-  product = await prismaClient.product.update({
-    where: { id, ownerId: decoded.userId },
-    data: {
-      name,
-      price,
-      description,
-    },
-  });
-  res.status(200).send(product);
 };
 
 export const deleteProduct = async (req: Request, res: Response) => {
-  const { id } = req.params;
-  if (!id) {
-    res.status(400).send({ message: "Product id is required" });
-    return;
+  try {
+    const { id } = req.params;
+    if (!id) {
+      res.status(400).send({ message: "Product id is required" });
+      return;
+    }
+    let product = await prismaClient.product.findFirst({ where: { id } });
+    if (!product) {
+      res.status(400).send({ message: "Product not found" });
+      return;
+    }
+    const token = req.headers["authorization"]?.split(" ")[1]!;
+    const decoded = jwt.verify(token, JWT_SECRET) as JwtPayload;
+    product = await prismaClient.product.findFirst({
+      where: { id, ownerId: decoded.userId },
+    });
+    if (!product) {
+      res
+        .status(401)
+        .send({ message: "You are not authorized to delete this product" });
+      return;
+    }
+    product = await prismaClient.product.delete({ where: { id } });
+    res.status(200).send(product);
+  } catch (e) {
+    console.log(e);
+    res.status(500).send("Internal server error");
   }
-  let product = await prismaClient.product.findFirst({ where: { id } });
-  if (!product) {
-    res.status(400).send({ message: "Product not found" });
-    return;
-  }
-  const token = req.headers["authorization"]?.split(" ")[1]!;
-  const decoded = jwt.verify(token, JWT_SECRET) as JwtPayload;
-  product = await prismaClient.product.findFirst({
-    where: { id, ownerId: decoded.userId },
-  });
-  if (!product) {
-    res
-      .status(401)
-      .send({ message: "You are not authorized to delete this product" });
-    return;
-  }
-  product = await prismaClient.product.delete({ where: { id } });
-  res.status(200).send(product);
 };
 
 export const getProduct = async (req: Request, res: Response) => {
-  const { id } = req.params;
-  if (!id) {
-    res.status(400).send({ message: "Product id is required" });
-    return;
+  try {
+    const { id } = req.params;
+    if (!id) {
+      res.status(400).send({ message: "Product id is required" });
+      return;
+    }
+    let product = await prismaClient.product.findFirst({ where: { id } });
+    if (!product) {
+      res.status(400).send({ message: "Product not found1" });
+      return;
+    }
+    res.status(200).send(product);
+  } catch (e) {
+    console.log(e);
+    res.status(500).send("Internal server error");
   }
-  let product = await prismaClient.product.findFirst({ where: { id } });
-  if (!product) {
-    res.status(400).send({ message: "Product not found1" });
-    return;
-  }
-  res.status(200).send(product);
 };
 
 export const searchProductByNameAndDescription = async (
   req: Request,
   res: Response
 ) => {
-  console.log("searchProductByNameAndDescription");
-  const { key } = req.body;
-  if (!key) {
-    res.status(400).send({ message: "Search key is required" });
-    return;
-  }
-  let product = await prismaClient.product.findMany({
-    where: {
-      OR: [
-        {
-          name: {
-            contains: key,
-            mode: "insensitive",
+  try {
+    const { key } = req.body;
+    if (!key) {
+      res.status(400).send({ message: "Search key is required" });
+      return;
+    }
+    let product = await prismaClient.product.findMany({
+      where: {
+        OR: [
+          {
+            name: {
+              contains: key,
+              mode: "insensitive",
+            },
           },
-        },
-        { description: { contains: key, mode: "insensitive" } },
-      ],
-    },
-  });
-  res.status(200).send(product);
+          { description: { contains: key, mode: "insensitive" } },
+        ],
+      },
+    });
+    res.status(200).send(product);
+  } catch (e) {
+    console.log(e);
+    res.status(500).send("Internal server error");
+  }
 };
 
 export const sortProduct = async (req: Request, res: Response) => {
-  let { key, type } = req.body;
-  if (!type) type = "asc";
   try {
+    let { key, type } = req.body;
+    if (!type) type = "asc";
     if (!key) {
       res.status(400).send({ message: "Sorting key required" });
       return;
